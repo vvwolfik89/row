7d8cc0135e552d4bc3476baadffccc66f2b4405b
vitaly <vitalyshaparau@gmail.com>
2020-07-02 01:06:02 +0300

    [ROW-19] - added type to coaches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200425201441) do
+ActiveRecord::Schema.define(version: 20200701185436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20200425201441) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "type_of_staff"
   end
 
   create_table "refinery_department_translations", force: :cascade do |t|
