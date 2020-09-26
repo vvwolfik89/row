69889d911680d8b96d923e86e2a273977687c82e
vitaly <vitalyshaparau@gmail.com>
2020-09-26 21:07:50 +0300

    [ROW-37] - added emp pagination

--- vendor/extensions/employees/app/controllers/refinery/employees/employees_controller.rb
+++ vendor/extensions/employees/app/controllers/refinery/employees/employees_controller.rb
@@ -8,7 +8,7 @@ module Refinery
       def index
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @employee in the line below:
-        @employees = Employee.with_filter(params)
+        @employees = Employee.with_filter(params).page(params[:page]).per_page(5)
         present(@page)
       end
 
