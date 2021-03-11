80ff58244b3288373953e0cd6de2388710390349
vitaly <vitalyshaparau@gmail.com>
2021-03-11 08:29:10 +0300

    [ROW-45] - added bel language

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var date = new Date(Date.now() + 86400e3);
-    date = date.toUTCString();
-    // setCookie.onclick = function (event) {
-    //     document.cookie = "popup=true; expires=" + date;
-    //     bg_popup.style.display = "none";
-    // };
-
-    if (document.cookie.match('popup')) {
-        bg_popup.style.display = "none";
-    }
-    else {
-        bg_popup.style.display = "flex";
-        setTimeout(function () {
-                document.cookie = "popup=true; expires=" + date;
-                bg_popup.style.display = "none";
-            },
-            15000
-        );
-    }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     var date = new Date(Date.now() + 86400e3);
+//     date = date.toUTCString();
+//     // setCookie.onclick = function (event) {
+//     //     document.cookie = "popup=true; expires=" + date;
+//     //     bg_popup.style.display = "none";
+//     // };
+//
+//     if (document.cookie.match('popup')) {
+//         bg_popup.style.display = "none";
+//     }
+//     else {
+//         bg_popup.style.display = "flex";
+//         setTimeout(function () {
+//                 document.cookie = "popup=true; expires=" + date;
+//                 bg_popup.style.display = "none";
+//             },
+//             15000
+//         );
+//     }
+// });
